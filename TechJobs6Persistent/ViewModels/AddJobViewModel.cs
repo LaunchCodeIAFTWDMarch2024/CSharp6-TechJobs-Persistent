4b1f258a928ddc4acce0ef29b950eaf3079c7586
Matthew Creek <matthew.creek27@gmail.com>
2024-06-21 12:32:42 -0500

    wip task 2

---
+++ TechJobs6Persistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobs6Persistent.Models;
+
+namespace TechJobs6Persistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string JobName { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem>? Employers { get; set; }
+
+        public AddJobViewModel(List<Employer> employers)
+        {
+            Employers = new List<SelectListItem>();
+
+            foreach (var employer in employers)
+            {
+                Employers.Add(
+                    new SelectListItem
+                    {
+                        Value = employer.Id.ToString(),
+                        Text = employer.Name
+                    }
+                );
+            }
+        }
+    }
+}
